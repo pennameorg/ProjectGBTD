55440a9c1bccca0fe50186645962bced16a822e9
Steven Woolard <steven@penname.org>
2026-04-25 10:25:42 -0400

    Initial Commit

---
+++ ProjectGBTD/ProjectGBTDApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectGBTDApp.swift
+//  ProjectGBTD
+//
+//  Created by Steven on 4/25/26.
+//
+
+import SwiftUI
+
+@main
+struct ProjectGBTDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
